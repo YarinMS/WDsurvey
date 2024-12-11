b498970d78cc7825fd74b39d0b7bd7f3ecb0501e
YarinMS <yarin-meir.shani@weizmann.ac.il>
2024-12-11 15:22:50 +0200

    2

--- loadFilesForPhotometry.m
+++ loadFilesForPhotometry.m
@@ -7,8 +7,7 @@ function AI = loadFilesForPhotometry(baseDir, cropID)
     visitDirs = dir(procDir);  % List all files/folders in procDir
     visitDirs = visitDirs([visitDirs.isdir]);  % Keep only directories
     visitDirs = visitDirs(~ismember({visitDirs.name}, {'.', '..'}));
-    visitDirs.folder
-    visitDirs.name
+
     
     % Find all relevant files
     filePattern = sprintf('*%03d_sci_proc_Image_1.fits', cropID);  % Assuming .hdf format
@@ -22,21 +21,21 @@ function AI = loadFilesForPhotometry(baseDir, cropID)
         visitDirPath = fullfile(procDir, visitDirs(i).name);  % Full path to the visit directory
         % Search for files matching the Crop ID pattern inside the visit directory
         cd(visitDirPath )
-        pwd
+ 
         fn    = FileNames.generateFromFileName(fullfile(visitDirPath, filePattern));
-        fn
+   
         AI = [AI  AstroImage.readFileNamesObj(fn)];
-        AI
+       
         % Add each found file to the fileList
 
     end
     cd(PWD)
     
     % Display results
-    if isempty(fileList)
+    if isempty(AI)
         fprintf('No files found for Crop ID %s.\n Path: %s \n', cropID,baseDir);
     else
-        fprintf('Found %d files for Crop ID %s.\n', numel(fileList), cropID);
+        fprintf('Found %d files for Crop ID %s.\n', numel(AI), cropID);
     end
 
 
