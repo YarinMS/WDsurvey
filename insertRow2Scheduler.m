0abcfc32ce6a7a8fa50a2ecb98a82e9f9e99b326
YarinMS <yarin-meir.shani@weizmann.ac.il>
2024-12-29 14:13:03 +0200

    fixx

---
+++ insertRow2Scheduler.m
@@ -0,0 +1,21 @@
+function S = insertRow2Scheduler(S,FieldName,RA,Dec,MountNum,StartJD,StopJD)
+
+
+exRow = S.searchFieldName('31UDR191');
+
+S.List.Catalog(end+1,:) = S.List.Catlaog(exRow,:);
+
+%
+S.List.Catalog.FieldName(end) = FieldName;
+
+S.List.Catalog.RA(end) = RA;
+
+S.List.Catalog.Dec(end) = Dec;
+
+S.List.Catalog.MountNum(end) = MountNum; 
+
+S.List.Catalog.StartJD(end) = StartJD;
+
+S.List.Catalog.StopJD(end) = StopJD;
+
+end
